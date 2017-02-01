705b14cbbac069aeba2963440f63536a8e8ff9be
keikhcheung <kei.khcheung@gmail.com>
2017-02-01 16:44:22 +0100

    Update infinite-scroll_V0.1.js

--- infinite-scroll_V0.1.js
+++ infinite-scroll_V0.1.js
@@ -219,8 +219,10 @@ var tumblrAutoPager = {
             var table_td = document.createElement('td');
             tAP.pp.appendChild(d);
             for (var i = 0; i < posts.length; i++) {
-                table_td(tAP.pp.appendChild(posts[i]));
+                document.createElement('td');
+                tAP.pp.appendChild(posts[i]);
               	tAP.autopagerize_page_element
+                document.createElement('/td');
             }
             var footer = $("footer");
             footer ? footer.parentNode.appendChild(footer) : null;
